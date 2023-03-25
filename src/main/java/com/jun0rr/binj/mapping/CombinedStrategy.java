c1c261445ef5d76e74bbe2141daaefa957e88425
Juno Roesler <juno.rr@gmail.com>
2023-03-25 20:08:53 -0300

    Created CombinedStrategy

---
+++ src/main/java/com/jun0rr/binj/mapping/CombinedStrategy.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jun0rr.binj.mapping;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ *
+ * @author F6036477
+ */
+public class CombinedStrategy<T> extends AbstractInvokeStrategy<T> {
+  
+  private final Map<Integer,InvokeStrategy<T>> strategies;
+  
+  public CombinedStrategy() {
+    this.strategies = new ConcurrentHashMap<>();
+  }
+  
+  public static <U> CombinedStrategy<U> newStrategy() {
+    return new CombinedStrategy<>();
+  }
+  
+  public CombinedStrategy<T> put(int weight, InvokeStrategy<T> strategy) {
+    this.strategies.put(weight, Objects.requireNonNull(strategy));
+    return this;
+  }
+  
+  public Map<Integer,InvokeStrategy<T>> strategies() {
+    return strategies;
+  }
+
+  @Override
+  public List<T> invokers(Class cls) {
+    List<T> fns = cache.get(cls);
+    if(fns == null) {
+      if(strategies.isEmpty()) {
+        throw new IllegalStateException("strategies is empty");
+      }
+      fns = strategies.entrySet().stream()
+          .sorted((a,b)->Integer.compare(a.getKey(), b.getKey()))
+          .map(Entry::getValue)
+          .flatMap(s->s.invokers(cls).stream())
+          .toList();
+      cache.put(cls, fns);
+    }
+    return fns;
+  }
+  
+}
---
+++ src/test/java/com/jun0rr/binj/test/TestCombinedStrategy.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jun0rr.binj.test;
+
+import com.jun0rr.binj.mapping.CombinedStrategy;
+import com.jun0rr.binj.mapping.ConstructFunction;
+import com.jun0rr.binj.mapping.MappingException;
+import com.jun0rr.binj.mapping.NoArgsConstructStrategy;
+import com.jun0rr.binj.mapping.FieldsOrderConstructStrategy;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+/**
+ *
+ * @author F6036477
+ */
+public class TestCombinedStrategy {
+  
+  @Test public void test() {
+    CombinedStrategy<ConstructFunction> strategy = CombinedStrategy.newStrategy();
+    strategy.put(3, new NoArgsConstructStrategy())
+        .put(2, new FieldsOrderConstructStrategy());
+    List<ConstructFunction> cs = strategy.invokers(Person.class);
+    String name = "John";
+    String last = "Doe";
+    LocalDate birth = LocalDate.now();
+    Map<String,Object> map = new HashMap<>();
+    map.put("name", name);
+    map.put("last", last);
+    map.put("birth", birth);
+    Optional<ConstructFunction> cct = cs.stream()
+        .filter(c->c.arguments().size() <= map.size())
+        .filter(c->c.arguments().stream().allMatch(s->map.keySet().stream().anyMatch(k->s.equals(k))))
+        .findFirst();
+    ConstructFunction cf = cct.orElseThrow(()->new MappingException("No ConstructFunction found for " + Person.class));
+    Person p = cf.create(map);
+    System.out.println(p);
+    Assertions.assertNotNull(p);
+    Assertions.assertEquals(name, p.name());
+    Assertions.assertEquals(last, p.last());
+    Assertions.assertEquals(birth, p.birth());
+  }
+  
+  
+  public static record Person(String name, String last, LocalDate birth) {
+  
+    public Person() {
+      this("Hello", "World", LocalDate.now());
+    }
+    
+  }
+  
+}
